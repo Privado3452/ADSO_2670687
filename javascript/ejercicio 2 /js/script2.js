58c74c4b8b2d783bb29ac638abc7e0edaa0aae41
Privado3452 <133520177+Privado3452@users.noreply.github.com>
2023-07-28 22:24:05 -0500

    realizacion de la guia

--- javascript/ejercicio 2 /js/script2.js
+++ javascript/ejercicio 2 /js/script2.js
@@ -17,9 +17,52 @@ alert("la ganacia del mes es:"+nsueldo) */
 
 /* ejercicio 4- */
 
-let sueldo = prompt("ingrese el valor del producto");
+/* let sueldo = prompt("ingrese el total de su compra");
 let desc = 25/100;
 let comp= parseInt(sueldo-desc)
-alert("el valor que debera pagar es:"+comp) 
+alert("el valor que debera pagar de su compra aplicand el descuento es:"+comp) 
 
-/* hola */
+  */
+
+/*let cap = prompt ("Ingrese el valor de su capital")
+let comi = 20/100;
+let pag= parseInt(cap+comi)
+alert("el valor que ganara con el 2% es de:"+pag) */
+
+/*let claf = prompt ("Ingrese las calificacion parciales.")
+let clf = prompt ("Ingrese la calificación del examen final.")
+let calf = prompt ("Ingrese la calificación de un trabajo final")
+let pro = 55/100
+let prom = 15/100
+let prm = 30/100
+let gan= parseInt(claf*pro)
+let ganal= parseInt(clf*prom)
+let gen= parseInt(calf*prm)
+let fin= parseInt(gan+ganal+gen)
+
+alert("su calificacion  de la materia es"+ fin)*/
+
+
+
+
+/*let pro = promt ("Ingrese el valor del articulo")
+let porc = 30/100;
+let gana= parseInt(cap+comi)
+alert("el valor que ganara con el 30% es:"+gana) */
+
+
+/*let km = promt ("Ingrese los kilometros recorridos")
+let min = promt ("Ingrese los minutos recorridos")
+let kil = (min*2000)
+let minu = (km*15000)
+let total= parseInt(kil+minu)
+alert("el valor que ganara con el 30% es:"+ total)*/
+
+
+/*let num1 = promt ("Ingrese un numero")
+let num2 = promt ("Ingrese un numero")
+let num3 = promt ("Ingrese un numero")
+let nu1 = (num1*num1)
+let nu2 = (num2*num2)
+let nu3 = (num3*num3)
+alert("la raiz cuadrada de "+num1+" es"+nu1 +", de"+num2 +"es"+nu2+ " y de"+ num3+" es"+nu3)*/
