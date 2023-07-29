d6fccd8abf8b906ae52273e615284f2e34f9c54e
Privado3452 <133520177+Privado3452@users.noreply.github.com>
2023-07-28 20:04:56 -0500

    actualizacion

---
+++ javascript/ejercicio 1 /js/script.js
@@ -0,0 +1,53 @@
+//console.log("Analisisy desarrollo de software");
+//alert("Analisisy desarrollo de software");
+/* este tipo de comentario se trabaja en diferentes lineas de codigo*/
+
+// para comentar en una linea de codigo
+
+//document.write("Analisisy desarrollo de software");
+
+//declarar variables var 
+
+//var edad=32;
+//let num=2.3;
+//Boolean="ADSO";
+//console.log(typeof Boolean); typeof es para definir el tipo de dato
+
+/* unir dos cadenas de texto
+const nom ="Duberney"
+const ape="Lopez"
+alert("Mi nombre es "+nom+" y mi apellido es "+ ape) */
+
+
+/* operadores aritmeticos
+const num1=5
+const num2=3
+
+const div = num1/num2
+
+parseInt(div) 
+
+//alert(num1*num2)
+alert("la division es:"+ div)*/
+
+let nom=prompt("Escriba el nombre y apellidos")
+let nom1=prompt("digite su edad")
+let nom2=prompt("digite su peso")
+let nom3=prompt("digite su direccion")
+
+alert("mi nombre es " + nom+ ", tengo " +nom1 +" años, "+   " peso "+nom2+" kg " + " y vivo en la cuidad de "+nom3)
+/* mostrar en una alerta nombre y apellidos completos, edad,peso y direccion*/ 
+
+//console.log("Mi edad es: "+edad); 
+// el simbolo "+" va funcionar para concatenar 
+ //document.write("Mi edad es: "+edad);
+ 
+//son dos tipos de valores 
+//alert("Mi edad es: "+edad);
+
+
+//const edad2=22;
+
+//let edad3=33;
+
+alert(num1*num2)
---
+++ javascript/ejercicio 2 /js/script2.js
@@ -0,0 +1,25 @@
+/* ejercicio 2  Dado el sueldo de un empleado imprimir en una alerta el nuevo sueldo del empleado si tuvo un aumneto del 10%
+
+let sueldo = prompt("ingrese el valor del sueldo");
+let psueldo = 10/100;
+let nsueldo = parseInt(sueldo)+parseInt(sueldo*psueldo)
+
+alert("El nuevo sueldo con el aumento es:"+nsueldo)*/
+
+/* ejercicio 3- un vendedor que tiene su sueldo base, recibe una comision del 30% del totalde las ventas del mes, el quiere saber cuanto ganara en un mes que tuvo 3 ventas
+
+let sueldo = prompt("ingrese el valor del sueldo base");
+let comi = 30/100;
+let valv = parseInt(comi)
+let venta = prompt("ingrese el total del valor de las ventas que realizo en el mes")
+let nsueldo = parseInt(sueldo)+parseInt(comi*venta)
+alert("la ganacia del mes es:"+nsueldo) */
+
+/* ejercicio 4- */
+
+let sueldo = prompt("ingrese el valor del producto");
+let desc = 25/100;
+let comp= parseInt(sueldo-desc)
+alert("el valor que debera pagar es:"+comp) 
+
+/* hola */
