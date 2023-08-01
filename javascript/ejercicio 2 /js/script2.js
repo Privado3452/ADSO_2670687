62bbf3d7e932179c966fb4c25572dde6fb9fa685
Privado3452 <133520177+Privado3452@users.noreply.github.com>
2023-07-31 21:23:45 -0500

    actualizacion

---
+++ javascript/ejercicio 2 /js/script2.js
@@ -0,0 +1,68 @@
+/* ejercicio 2  Dado el sueldoo de un empleado imprimir en una alerta el nuevo sueldo del empleado si tuvo un aumneto del 10%
+
+let sueldo = prompt("ingrese el valor del sueldo");
+let psueldo = 10/100;
+let nsueldo = parseInt(sueldo)+parseInt(sueldo*psueldo)
+
+alert("El nuevo sueldo con el aumento es:"+nsueldo)*/
+
+/* ejercicio 3- un vendedor que tiene su sueldo base, recibe una comision del 30% del totalde las ventas del mes, el quiere saber cuanto ganara en un mes que tuvo 3 ventas
+
+let sueldo = prompt("ingrese el valor del sueldo base");
+let comi = 30/100;
+let valv = parseInt(comi)
+let venta = prompt("ingrese el total del valor de las ventas que realizo en el mes")
+let nsueldo = parseInt(sueldo)+parseInt(comi*venta)
+alert("la ganacia del mes es:"+nsueldo) */
+
+/* ejercicio 4- */
+
+/* let sueldo = prompt("ingrese el total de su compra");
+let desc = 25/100;
+let comp= parseInt(sueldo-desc)
+alert("el valor que debera pagar de su compra aplicand el descuento es:"+comp) 
+
+  */
+
+/*let cap = prompt ("Ingrese el valor de su capital")
+let comi = 20/100;
+let pag= parseInt(cap+comi)
+alert("el valor que ganara con el 2% es de:"+pag) */
+
+/*let claf = prompt ("Ingrese las calificacion parciales.")
+let clf = prompt ("Ingrese la calificación del examen final.")
+let calf = prompt ("Ingrese la calificación de un trabajo final")
+let pro = 55/100
+let prom = 15/100
+let prm = 30/100
+let gan= parseInt(claf*pro)
+let ganal= parseInt(clf*prom)
+let gen= parseInt(calf*prm)
+let fin= parseInt(gan+ganal+gen)
+
+alert("su calificacion  de la materia es"+ fin)*/
+
+
+
+
+/*let pro = promt ("Ingrese el valor del articulo")
+let porc = 30/100;
+let gana= parseInt(cap+comi)
+alert("el valor que ganara con el 30% es:"+gana) */
+
+
+/*let km = promt ("Ingrese los kilometros recorridos")
+let min = promt ("Ingrese los minutos recorridos")
+let kil = (min*2000)
+let minu = (km*15000)
+let total= parseInt(kil+minu)
+alert("el valor que ganara con el 30% es:"+ total)*/
+
+
+/*let num1 = promt ("Ingrese un numero")
+let num2 = promt ("Ingrese un numero")
+let num3 = promt ("Ingrese un numero")
+let nu1 = (num1*num1)
+let nu2 = (num2*num2)
+let nu3 = (num3*num3)
+alert("la raiz cuadrada de "+num1+" es"+nu1 +", de"+num2 +"es"+nu2+ " y de"+ num3+" es"+nu3)*/
