7311b49290fc45fabbcb79db7b1e1c8cda0c3312
Privado3452 <133520177+Privado3452@users.noreply.github.com>
2023-09-19 22:14:37 -0500

    ejercicios de control de javascript

---
+++ javascript/ejercicio de control js/js/ejercicio.js
@@ -0,0 +1,165 @@
+
+/*
+let edad = prompt("ingrese la edad");
+
+if (edad >=18){  
+     alert("Usted es mayor de edad"); 
+ } else if(edad <=18){  
+     alert("menor de edad"); 
+ }
+*/
+
+/* let claf = prompt ("Ingrese las calificacion .")
+let clf = prompt ("Ingrese la calificación .")
+let calf = prompt ("Ingrese la calificación ")
+let pro = 55/100
+let prom = 15/100
+let prm = 30/100
+let gan= parseInt(claf*pro)
+let ganal= parseInt(clf*prom)
+let gen= parseInt(calf*prm)
+let fin= parseInt(gan+ganal+gen)
+
+alert("su calificacion  de la materia es"+ fin)
+
+if (fin >=7){  
+    alert("Aprobado"); 
+} else if(fin <=7){  
+    alert(" Reprobado"); 
+}
+*/
+
+/*var A = prompt ("ingrese un numero:");
+var B = prompt ("ingrese un numero:");
+
+    if  (A<B){
+        alert(A+" es menor que "+B);
+} 
+    else if (A>B){
+        alert(A+" es mayor que "+B);
+}
+    else (A==B){
+        alert ('Es igual los dos numeros')
+}
+*/
+
+/*var A = prompt ("ingrese un numero:");
+var B = prompt ("ingrese un numero:");
+
+    if  (A<B){
+        alert(A+" "+B);
+} 
+    else if (A>B){
+        alert(B+" "+A);
+}
+    else (A==B){
+        alert (A+''+B)
+}
+*/
+
+/*var P = prompt ("ingrese su peso corporal:");
+
+    if  (P<40){
+        alert(" Bajo peso ");
+} 
+    else if (P<70){
+        alert(" Peso promedio ");
+}
+    else if (P>71){
+        alert ('Sobrepeso')
+} */
+
+
+ const diccionario = {
+    1: "Lunes",
+    2: "Martes",
+    3: "Miercoles",
+    4: "Jueves",
+    5: "Viernes",
+    6: "Sabado",
+    7: "Domingo"
+
+}
+ 
+let nume1 = parseInt(prompt("digite el numero: "));  
+ 
+
+
+  
+
+
+
+
+
+
+
+
+
+/* ejercicio 3- un vendedor que tiene su sueldo base, recibe una comision del 30% del totalde las ventas del mes, el quiere saber cuanto ganara en un mes que tuvo 3 ventas
+
+let sueldo = prompt("ingrese el valor del sueldo base");
+let comi = 30/100;
+let valv = parseInt(comi)
+let venta = prompt("ingrese el total del valor de las ventas que realizo en el mes")
+let nsueldo = parseInt(sueldo)+parseInt(comi*venta)
+alert("la ganacia del mes es:"+nsueldo) */
+
+/* ejercicio 4- */
+
+/* let sueldo = prompt("ingrese el total de su compra");
+let desc = 25/100;
+let comp= parseInt(sueldo-desc)
+alert("el valor que debera pagar de su compra aplicand el descuento es:"+comp) 
+
+  */
+
+/*let cap = prompt ("Ingrese el valor de su capital")
+let comi = 20/100;
+let pag= parseInt(cap+comi)
+alert("el valor que ganara con el 2% es de:"+pag) */
+
+ 
+
+
+
+
+
+/*let pro = promt ("Ingrese el valor del articulo")
+let porc = 30/100;
+let gana= parseInt(cap+comi)
+alert("el valor que ganara con el 30% es:"+gana) */
+
+
+/*let km = promt ("Ingrese los kilometros recorridos")
+let min = promt ("Ingrese los minutos recorridos")
+let kil = (min*2000)
+let minu = (km*15000)
+let total= parseInt(kil+minu)
+alert("el valor que ganara con el 30% es:"+ total)*/
+
+
+/*let num1 = promt ("Ingrese un numero")
+let num2 = promt ("Ingrese un numero")
+let num3 = promt ("Ingrese un numero")
+let nu1 = (num1*num1)
+let nu2 = (num2*num2)
+let nu3 = (num3*num3)
+alert("la raiz cuadrada de "+num1+" es"+nu1 +", de"+num2 +"es"+nu2+ " y de"+ num3+" es"+nu3)*/
+
+/* let opcion = prompt("1. suma 2. resta 3. division 4. multiplcacion"); 
+ let nume1 = prompt("digite el numero 1"); 
+ let nume2 = prompt("digite el numero 2"); 
+  
+ if (opcion==1){ 
+     suma=parseInt(nume1)+parseInt(nume2); 
+     alert(suma); 
+ } else if(opcion==2){ 
+     resta=parseInt(nume1)-parseInt(nume2); 
+     alert(resta); 
+ } else if(opcion==3){ 
+     division=parseInt(nume1)/parseInt(nume2); 
+     alert(division); 
+ } else { 
+     multiplicacion=parseInt(nume1)*parseInt(nume2); 
+     alert(multiplicacion); 
+ }*/
